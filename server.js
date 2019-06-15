d35221eb74cb8392dad4d8881dd266d849802bbd
thajedster <jedlawrencecheng@gmail.com>
2019-06-15 10:22:13 -0400

    added index html and scraper route and get route for articles

--- server.js
+++ server.js
@@ -16,10 +16,40 @@ app.use(express.json());
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 //routes
 
+axios.get('https://www.apnews.com/').then((response) => {
+    var $ = cheerio.load(response.data);
+
+    $(".c0125").each(function (i, element) {
+        result = {}
+        //result.title = $(this).children(".0126").children(".0127").text();
+        result.link = $(this).children(".c0126").attr("href");
+
+        db.Article.create(result).then(function (dbArticle) {
+            console.log(dbArticle);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+});
+
+
+
+app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 
 //server start
